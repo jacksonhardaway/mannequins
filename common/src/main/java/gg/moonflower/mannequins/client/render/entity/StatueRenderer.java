327cbce40d511713b3cab8fdbc47acaf47d15a68
Jackson <jackson.hardaway@gmail.com>
2022-01-01 11:47:56 -0800

    Renamed stone mannequin to statue

--- common/src/main/java/gg/moonflower/mannequins/client/render/entity/StatueRenderer.java
+++ common/src/main/java/gg/moonflower/mannequins/client/render/entity/StatueRenderer.java
@@ -1,16 +1,18 @@
 package gg.moonflower.mannequins.client.render.entity;
 
+import gg.moonflower.mannequins.client.render.model.BasicMannequinModel;
+import gg.moonflower.mannequins.client.render.model.MannequinsModelLayers;
 import gg.moonflower.mannequins.client.render.model.StatueModel;
 import gg.moonflower.mannequins.common.entity.Statue;
 import gg.moonflower.mannequins.core.Mannequins;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
 public class StatueRenderer extends AbstractMannequinRenderer<Statue> {
     public static final ResourceLocation TEXTURE = new ResourceLocation(Mannequins.MOD_ID, "textures/entity/statue.png");
 
-    public StatueRenderer(EntityRenderDispatcher dispatcher) {
-        super(dispatcher, new StatueModel());
+    public StatueRenderer(EntityRendererProvider.Context context) {
+        super(context, new StatueModel(context.bakeLayer(MannequinsModelLayers.STATUE)), new BasicMannequinModel<>(context.bakeLayer(MannequinsModelLayers.STATUE_INNER_ARMOR)), new BasicMannequinModel<>(context.bakeLayer(MannequinsModelLayers.STATUE_OUTER_ARMOR)));
     }
 
     @Override
--- common/src/main/java/gg/moonflower/mannequins/client/render/model/MannequinsModelLayers.java
+++ common/src/main/java/gg/moonflower/mannequins/client/render/model/MannequinsModelLayers.java
@@ -5,13 +5,13 @@ import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.resources.ResourceLocation;
 
 public class MannequinsModelLayers {
-    public static final ModelLayerLocation MANNEQUIN = create("mannequin", "main");
+    public static final ModelLayerLocation MANNEQUIN = create("mannequin");
     public static final ModelLayerLocation MANNEQUIN_INNER_ARMOR = create("mannequin", "inner_armor");
     public static final ModelLayerLocation MANNEQUIN_OUTER_ARMOR = create("mannequin", "outer_armor");
 
-    public static final ModelLayerLocation STONE_MANNEQUIN = create("stone_mannequin", "main");
-    public static final ModelLayerLocation STONE_MANNEQUIN_INNER_ARMOR = create("stone_mannequin", "inner_armor");
-    public static final ModelLayerLocation STONE_MANNEQUIN_OUTER_ARMOR = create("stone_mannequin", "outer_armor");
+    public static final ModelLayerLocation STATUE = create("statue");
+    public static final ModelLayerLocation STATUE_INNER_ARMOR = create("statue", "inner_armor");
+    public static final ModelLayerLocation STATUE_OUTER_ARMOR = create("statue", "outer_armor");
 
     public static ModelLayerLocation create(String model) {
         return create(model, "main");
